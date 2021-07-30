e03a372ec708ac5e5e5549ec7abf0ba058bfce57
Flugstein <flugstein@protonmail.com>
2021-07-30 14:37:56 +0200

    add list of retweeters instead of retweet_count

--- sim_features.py
+++ sim_features.py
@@ -1,7 +1,7 @@
 # Extracting Simulation Features from Preprocessed Twitter Data
 #
 # Author: Florian Lugstein (flugstein@cs.sbg.ac.at)  
-# Date: 2020-07-29
+# Date: 2021-07-29
 
 import sys
 import numpy as np
@@ -34,38 +34,46 @@ sf = pd.DataFrame()
 def binary_bool(value):
     return 1 if value else 0
 
+def get_retweeters_adjlist(id_):
+    return list(tf[tf['retweet_of'] == id_]['user.adjlist_id'])
+
+def get_retweeters_metis(id_):
+    return list(tf[tf['retweet_of'] == id_]['user.metis_id'])
+
 
 # Filter tweets
 
 print('{} source tweets without author removed'.format(tf[((tf['user.adjlist_id'] == 0) | (tf['user.metis_id'] == 0)) & (tf['source'] == True)].shape[0]))
 tf = tf[tf['user.adjlist_id'] != 0]  # only tweets with author
 tf = tf[tf['user.metis_id'] != 0]  # only tweets with author
-tf = tf[tf['source'] == True]  # only source tweets
+tfs = tf[tf['source'] == True]  # only source tweets
 
 
 # Features
 
-sf['author_adjlist'] = tf['user.adjlist_id']
+sf['author_adjlist'] = tfs['user.adjlist_id']
 
-sf['author_metis'] = tf['user.metis_id']
+sf['author_metis'] = tfs['user.metis_id']
 
-sf['verified'] = tf['user.verified'].apply(binary_bool)
+sf['verified'] = tfs['user.verified'].apply(binary_bool)
 
-sf['activity'] = (tf['user.activity'] >= tf['user.mean_activity']).apply(binary_bool)
+sf['activity'] = (tfs['user.activity'] >= tfs['user.mean_activity']).apply(binary_bool)
 
-sf['defaultprofile'] = tf['user.default_profile'].apply(binary_bool)
+sf['defaultprofile'] = tfs['user.default_profile'].apply(binary_bool)
 
-sf['userurl'] = tf['user.url'].apply(binary_bool)
+sf['userurl'] = tfs['user.url'].apply(binary_bool)
 
-sf['hashtag'] = tf['hashtag'].apply(binary_bool)
+sf['hashtag'] = tfs['hashtag'].apply(binary_bool)
 
-sf['tweeturl'] = tf['url'].apply(binary_bool)
+sf['tweeturl'] = tfs['url'].apply(binary_bool)
 
-sf['mentions'] = tf['mention'].apply(binary_bool)
+sf['mentions'] = tfs['mention'].apply(binary_bool)
 
-sf['media'] = tf['media'].apply(binary_bool)
+sf['media'] = tfs['media'].apply(binary_bool)
 
-sf['retweets'] = tf['retweeted_count']
+sf['retweeter_adjlist'] = tfs['id'].apply(get_retweeters_adjlist)
+
+sf['retweeter_metis'] = tfs['id'].apply(get_retweeters_metis)
 
 
 ##############################################################################
